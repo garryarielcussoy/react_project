743db62dcd83098706e9c8e27e0de0945239e09d
Garry Ariel <garry@alterra.id>
2020-01-09 19:31:46 +0700

    Preparation Done

--- react_project/src/route/MainRoute.js
+++ react_project/src/route/MainRoute.js
@@ -4,14 +4,20 @@ import {Link, BrowserRouter, Switch, Route} from 'react-router-dom'
 import {Provider} from 'unistore/react'
 import {store, actions} from '../global/store'
 
+import Home from '../pages/Home'
+
 const MainRoute = () => {
     return (
         <Provider store={store}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path='/' component={Home} />
+                    {/* <Route exact path='/profile' component={Profile} />
+                    <Route exact path='/login' component={Login} /> */}
                 </Switch>
             </BrowserRouter>
         </Provider>
     )
 }
+
+export default MainRoute
