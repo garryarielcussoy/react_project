93a7c05199a78d8da6565edb17639f7ba6342817
bimon-alta <57992093+bimon-alta@users.noreply.github.com>
2020-01-09 23:46:34 +0700

    Revert "Login page"

--- react_project/src/pages/Home.js
+++ react_project/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from '../components/Header'
-import HomePageComponent from '../components/HomePageComponent';
 
 import {withRouter} from 'react-router-dom'
 import {store, actions} from '../global/store'
@@ -9,10 +8,7 @@ import { connect } from "unistore/react";
 class Home extends React.Component{
     render(){
         return (
-            <React.Fragment>
-                <Header />
-                <HomePageComponent />
-            </React.Fragment>
+            <Header />
         )
     }
 }
--- react_project/src/route/MainRoute.js
+++ react_project/src/route/MainRoute.js
@@ -2,11 +2,9 @@ import React from 'react'
 import {Link, BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import {Provider} from 'unistore/react'
-import {store, actions} from '../global/storeSetyo'
+import {store, actions} from '../global/store'
 
 import Home from '../pages/Home'
-import Profile from '../pages/profile'
-import Login from '../pages/Login';
 
 const MainRoute = () => {
     return (
@@ -14,8 +12,8 @@ const MainRoute = () => {
             <BrowserRouter>
                 <Switch>
                     <Route exact path='/' component={Home} />
-                    <Route exact path='/profile' component={Profile} />]}
-                    <Route exact path='/login' component={Login} /> 
+                    {/* <Route exact path='/profile' component={Profile} />
+                    <Route exact path='/login' component={Login} /> */}
                 </Switch>
             </BrowserRouter>
         </Provider>
