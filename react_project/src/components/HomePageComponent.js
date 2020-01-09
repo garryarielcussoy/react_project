bc3629a5cdd028d3b3c41e01ca73197a8095ae49
Garry Ariel <garry@alterra.id>
2020-01-09 22:35:27 +0700

    HomePage Done

--- react_project/src/components/HomePageComponent.js
+++ react_project/src/components/HomePageComponent.js
@@ -13,9 +13,9 @@ class HomePageComponent extends React.Component{
                     <div className='col-md-2'></div>
                     <div className='col-md-8 info-kota-information'>
                         <p className='information-content'>
-                            Selamat Datang di INFO KOTA. Melalui aplikasi ini, kamu bisa caritau banyak hal
-                            seputar suatu kota, mulai dari tempat makan, bioskop, sampai tingkat biaya hidup!
-                            Untuk bisa menikmati aplikasi ini, kamu cukup login terlebih dahulu &#x1F31D;
+                            Selamat Datang di INFO KOTA. Melalui aplikasi ini, kamu bisa caritau banyak informasi penting
+                            seputar suatu kota, mulai dari rumah sakit, universitas, sampai tingkat biaya hidupnya!
+                            Untuk bisa menikmati aplikasi ini, kamu cukup login terlebih dahulu, simpel bukan? &#x1F31D;
                         </p>
                     </div>
                     <div className='col-md-2'></div>
