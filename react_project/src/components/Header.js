83447da60d6961e6307f102e1e3fef79fa8f7fbd
bimon-alta <bimon@alterra.id>
2020-01-09 21:30:48 +0700

    login first commit

--- react_project/src/components/Header.js
+++ react_project/src/components/Header.js
@@ -5,13 +5,13 @@ import '../styles/main.css'
 class Header extends React.Component{
     render(){
         return (
-            <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                <a class="navbar-brand" href="#">Info Kota</a>
+            <nav className="navbar navbar-default">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                <a className="navbar-brand" href="#">Info Kota</a>
                 </div>
-                <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
+                <ul className="nav navbar-nav">
+                <li className="active"><a href="#">Home</a></li>
                 <li><a href="#">Login</a></li>
                 </ul>
             </div>
