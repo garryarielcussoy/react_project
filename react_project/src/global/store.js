7cfe3f1102b04095b66103264d334405513cfa6c
Garry Ariel <garry@alterra.id>
2020-01-10 00:29:23 +0700

    Edit store

--- react_project/src/global/store.js
+++ react_project/src/global/store.js
@@ -11,6 +11,31 @@ const allState = {
         profileImage: '',
         isLogin: false,
         isRemembered: false
+    },
+    resultTeleportScore: {
+
+    },
+    fourSquareRequirement: {
+        clientId: 'SVNLQVD0ST2L1E5JE5PU0S0Z2MIVPOI15DEEWFB41EDRXBTZ',
+        clientSecret: '004QUQHQLHNYEZK3LY0N3IC3E0LQ34HU5WUYAEDIPWMNEALH',
+        v: '20190901',
+        near: 'malang',
+        limit: 5,
+        categoryId: {
+            university: '4bf58dd8d48988d1ae941735',
+            sportFacility: '4f4528bc4b90abdf24c9de85',
+            hospital: '4bf58dd8d48988d196941735',
+            tempatIbadah: '4bf58dd8d48988d131941735',
+            mall: '4bf58dd8d48988d1fd941735'
+        }
+    },
+
+    resultFoursquare: {
+        id: '',
+        name: '',
+        address: '',
+        photos: '',
+        rating: ''
     }
 };
 
